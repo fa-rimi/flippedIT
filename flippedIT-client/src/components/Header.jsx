0726144f0e7b3314b640e8c8cfc8b0a38c3b9fee
rimi <a.fatimaarimi@gmail.com>
2023-09-12 19:18:11 -0400

    // eslint-disable-next-line no-unused-vars

--- flippedIT-client/src/components/Header.jsx
+++ flippedIT-client/src/components/Header.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
 
-const Header = () => {
+const Header = ({ onStartGame, clickCount, timer }) => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="intro">
+        <h1>flip IT</h1>
+        <p>flip the cards <br /> match the colors</p>
+      </div>
+      <div className="mode">
+        <button type="button" id="easy" onClick={() => onStartGame('easy')}>
+          Easy
+        </button>
+        <button type="button" id="medium" onClick={() => onStartGame('medium')}>
+          Medium
+        </button>
+        <button type="button" id="hard" onClick={() => onStartGame('hard')}>
+          Hard
+        </button>
+        <button
+          type="button"
+          id="harder"
+          onClick={() => onStartGame('harder')}
+        >
+          Harder
+        </button>
+      </div>
+      <div className="stats">
+        <div id="click-count">Click Count: {clickCount}</div>
+        <div id="timer">Time: {timer}</div>
+      </div>
+      <div className="links">
+        <a href="https://github.com/fa-rimi">
+          <img src="images/logos/icons8-github-48.png" alt="" />
+        </a>
+        <a href="credits/credit.html">credits</a>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- flippedIT-client/src/pages/Homepage.jsx
+++ flippedIT-client/src/pages/Homepage.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import Header from "../components/Header.jsx";
 import GameBoard from "../components/GameBoard";
