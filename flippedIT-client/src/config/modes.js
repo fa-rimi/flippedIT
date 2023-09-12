17fae5341199cbbc988d36d54928bdcc99f6c75c
rimi <a.fatimaarimi@gmail.com>
2023-09-12 19:49:41 -0400

    modes config for game levels

---
+++ flippedIT-client/src/config/modes.js
@@ -0,0 +1,29 @@
+const modes = [
+    {
+      id: 'easy',
+      label: 'Easy',
+      rows: 2,
+      columns: 2,
+      message: "Easy mode activated"
+    //   timeLimitInSeconds: 5,
+    },
+    {
+      id: 'medium',
+      label: 'Medium',
+      rows: 4,
+      columns: 4,
+      message: "Medium mode activated"
+    //   timeLimitInSeconds: 60,
+    },
+    {
+      id: 'hard',
+      label: 'Hard',
+      rows: 6,
+      columns: 6,
+      message: "Hard mode activated"
+    //   timeLimitInSeconds: 120,
+    },
+  ];
+  
+  export default modes;
+  
